39b160867ee00a826a8a04abdc54520ffcd2cd87
Sengo Basar <bowjok5@example.com>
2025-08-25 19:17:07 +0530

    Update supabase client to use environment variables

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://faixqfttqqfzfozxzpwv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhaXhxZnR0cXFmemZvenh6cHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNTUyOTAsImV4cCI6MjA3MTYzMTI5MH0.Qn0TeyBrzt9mDbJJhRTq-XmiSCpCCOCtfgECvcDpeiw'; // replace with your anon key or from environment variables
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
